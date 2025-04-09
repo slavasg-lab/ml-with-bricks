0fbdfaeb177900bf98a3b9451dae2f4417b7e260
Viacheslav Sydora <viacheslav.sydora@gmail.com>
2025-04-09 15:58:05 +0200

    minor deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({ babel: { plugins: ['styled-components'], babelrc: false, } })],
-  base: "/ml-with-bricks"
+  base: "/ml-with-bricks/"
 })

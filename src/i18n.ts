bbbdef2c45e33d8bd1c51c7e60b6344df8650d37
Viacheslav Sydora <viacheslav.sydora@gmail.com>
2025-04-09 17:32:32 +0200

    minor deploy fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,18 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import HttpApi from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector"
+import LanguageDetector from "i18next-browser-languagedetector";
+import translationEN from "./locales/en.json";
+import translationDE from "./locales/de.json";
 
-const backendOptions = [{
-    loadPath: '/locales/{{lng}}.json',
-}];
 
-i18next.use(HttpApi).use(LanguageDetector).use(initReactI18next).init({
+i18next.use(LanguageDetector).use(initReactI18next).init({
     debug: true,
     load: 'languageOnly',
     fallbackLng: "en",
-    backend: backendOptions
+    resources: {
+        en: translationEN,
+        de: translationDE
+    }
 });
 
 export default i18next;

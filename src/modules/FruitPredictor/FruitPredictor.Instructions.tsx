595426f2751d97fa9ed3f0a30bcef5da5e39ee26
Viacheslav Sydora <viacheslav.sydora@gmail.com>
2025-06-01 13:33:03 +0200

    implemented Michael`s feedback to fruit predictor page

--- src/modules/FruitPredictor/FruitPredictor.Instructions.tsx
+++ src/modules/FruitPredictor/FruitPredictor.Instructions.tsx
@@ -1,65 +1,51 @@
 import { styled } from "styled-components";
 import Typography from "../../components/Typography/Typography";
-import InTextLink from "../../components/Links/InTextLink";
 import { useTranslation } from "react-i18next";
-import i18next from "i18next";
 
 const FruitPredictorInstructions = () => {
   const { t } = useTranslation();
 
   return (
     <Wrapper>
-      <Typography.Text>{t("FruitPredictorInstructions.intro")}</Typography.Text>
       <Typography.Text>
-        {t("FruitPredictorInstructions.followVideo")}
-        <InTextLink to={"https://youtu.be/G8OpubRhUhU"}>
-          {t("video")}
-        </InTextLink>
-        {t("FruitPredictorInstructions.followVideoPart2")}
+        {t("FruitPredictorInstructions.paragraph1")}
       </Typography.Text>
+      <Typography.Text $markdown>
+        {t("FruitPredictorInstructions.paragraph2")}
+      </Typography.Text>
+
       <Typography.Text $bold>
-        {t("FruitPredictorInstructions.assemblyTitle")}
+        {t("FruitPredictorInstructions.step1.heading")}
       </Typography.Text>
-      <Typography.Text>
-        {t("FruitPredictorInstructions.assemblyText1Part1")}
-        <InTextLink
-          to={`/ml-with-bricks/materials/${i18next.language}/assembly/fruit-predictor.pdf`}
-        >
-          {t("these instructions")}
-        </InTextLink>
-        {t("FruitPredictorInstructions.assemblyText1Part2")}
+      <Typography.Text $markdown>
+        {t("FruitPredictorInstructions.step1.paragraph1")}
       </Typography.Text>
-      <Typography.Text>
-        {t("FruitPredictorInstructions.assemblyText2Part1")}
-        <InTextLink to={`/ml-with-bricks/materials/${i18next.language}/connection.pdf`}>
-          {t("pdf")}
-        </InTextLink>
-        {t("FruitPredictorInstructions.assemblyText2Part2")}
-        <InTextLink to={"https://youtu.be/ZsviP40PnuA"}>
-          {t("video")}
-        </InTextLink>
-        {t("FruitPredictorInstructions.assemblyText2Part3")}
+      <Typography.Text $markdown>
+        {t("FruitPredictorInstructions.step1.paragraph2")}
       </Typography.Text>
+
       <Typography.Text $bold>
-        {t("FruitPredictorInstructions.dataCollectionTitle")}
+        {t("FruitPredictorInstructions.step2.heading")}
       </Typography.Text>
-      <Typography.Text>
-        {t("FruitPredictorInstructions.dataCollectionText")}
+      <Typography.Text $markdown>
+        {t("FruitPredictorInstructions.step2.paragraph1")}
       </Typography.Text>
+
       <Typography.Text $bold>
-        {t("FruitPredictorInstructions.predictionTitle")}
+        {t("FruitPredictorInstructions.step3.heading")}
       </Typography.Text>
-      <Typography.Text>
-        {t("FruitPredictorInstructions.predictionText1")}
+      <Typography.Text $markdown>
+        {t("FruitPredictorInstructions.step3.paragraph1")}
       </Typography.Text>
-      <Typography.Text>
-        {t("FruitPredictorInstructions.predictionText2")}
+      <Typography.Text $markdown>
+        {t("FruitPredictorInstructions.step3.paragraph2")}
       </Typography.Text>
+
       <Typography.Text $bold>
-        {t("FruitPredictorInstructions.thingsToTryTitle")}
+        {t("FruitPredictorInstructions.step4.heading")}
       </Typography.Text>
-      <Typography.Text>
-        {t("FruitPredictorInstructions.thingsToTryText")}
+      <Typography.Text $markdown>
+        {t("FruitPredictorInstructions.step4.paragraph1")}
       </Typography.Text>
     </Wrapper>
   );
